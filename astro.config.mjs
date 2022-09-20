f40319a2cf646cc9d2744fd0ca78dc4c60ae3fef
jafar <aljuneidi.jafar@gmail.com>
2022-09-20 21:02:48 +0300

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), image()]
+  integrations: [mdx(), sitemap(), react(), image(), tailwind()]
 })
