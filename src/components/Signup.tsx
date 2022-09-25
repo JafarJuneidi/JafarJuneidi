56b5b70778c80a7cd47ac8f4e1137d167306a8d0
jafar <aljuneidi.jafar@gmail.com>
2022-09-25 17:03:33 +0300

    deleted example blogs & Modified Newsletter dark mode css (with help from Dan Abramov's blog styling)

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -8,134 +8,115 @@ const form = {
 }
 export default function Signup() {
   return (
-    <div className='mt-6'>
-      <script src='https://f.convertkit.com/ckjs/ck.5.js'></script>
-      <form
-        action={`https://app.convertkit.com/forms/${form.id}/subscriptions`}
-        className='seva-form formkit-form'
-        method='post'
-        data-sv-form='3644704'
-        data-uid='92f8ea0121'
-        data-format='inline'
-        data-version='5'
-        min-width='400 500 600 700 800'
-        style={{ backgroundColor: 'rgb(255, 255, 255)', borderRadius: '6px' }}
-      >
-        <div data-style='full'>
+    <form
+      action={`https://app.convertkit.com/forms/${form.id}/subscriptions`}
+      className='seva-form formkit-form mt-6 rounded-md shadow shadow-gray-300 bg-gray-50 dark:bg-zinc-800 dark:shadow-gray-800'
+      method='post'
+      data-sv-form='3644704'
+      data-uid='92f8ea0121'
+      data-format='inline'
+      data-version='5'
+      min-width='400 500 600 700 800'
+    // style={{ backgroundColor: 'rgb(255, 255, 255)', borderRadius: '6px', boxShadow: dark }}
+    >
+      <div data-style='full'>
+        <div data-element='column' className='formkit-column'>
+          <div className='formkit-background' style={{ opacity: '0.3' }}></div>
           <div
-            data-element='column'
-            className='formkit-column'
-            style={{ backgroundColor: 'rgb(249, 250, 251)' }}
+            className='formkit-header'
+            data-element='header'
+            style={{
+              color: 'rbb(77, 77, 77)',
+              fontSize: '25px',
+              fontWeight: 700
+            }}
           >
-            <div
-              className='formkit-background'
-              style={{ opacity: '0.3' }}
-            ></div>
-            <div
-              className='formkit-header'
-              data-element='header'
-              style={{
-                color: 'rbb(77, 77, 77)',
-                fontSize: '16px',
-                fontWeight: 700
-              }}
-            >
-              <h2>{form.title}</h2>
-            </div>
-            <div
-              className='formkit-subheader'
-              data-element='subheader'
-              style={{ color: 'rgb(104, 104, 104)', fontSize: '15px' }}
-            >
-              <p>{form.subTitle}</p>
-            </div>
-            <div
-              className='formkit-image formkit-image relative focus:outline-none'
-              role='button'
-              // tabindex='0'
-            >
-              <img
-                className='cursor-pointer focus:outline-blue '
-                src='https://embed.filekitcdn.com/e/8z8RseeFJ6cFGgheuTWKHN/rCC6cTJVbiDvbHM1MFZm6Z'
-                style={{ maxWidth: '100%' }}
-              />
-            </div>
+            <h2>{form.title}</h2>
           </div>
-
-          <div data-element='column' className='formkit-column'>
-            <ul
-              className='formkit-alert formkit-alert-error'
-              data-element='errors'
-              data-group='alert'
-            ></ul>
-            <div data-element='fields' className='seva-fields formkit-fields'>
-              <div className='formkit-field'>
-                <input
-                  className='formkit-input'
-                  aria-label='First Name'
-                  name='fields[first_name]'
-                  required={true}
-                  placeholder='First Name'
-                  type='text'
-                  style={{
-                    color: 'rgb(0, 0, 0)',
-                    borderColor: 'rgb(227, 227, 227)',
-                    borderRadius: '4px',
-                    fontWeight: 400
-                  }}
-                />
-              </div>
-              <div className='formkit-field'>
-                <input
-                  className='formkit-input'
-                  name='email_address'
-                  aria-label='Email Address'
-                  placeholder='Email Address'
-                  required={true}
-                  type='email'
-                  style={{
-                    color: 'rgb(0, 0, 0)',
-                    borderColor: 'rgb(227, 227, 227)',
-                    borderRadius: '4px',
-                    fontWeight: 400
-                  }}
-                />
-              </div>
-              <button
-                data-element='submit'
-                className='formkit-submit formkit-submit'
-                style={{
-                  color: 'rgb(255, 255, 255)',
-                  backgroundColor: 'rgb(22, 119, 190)',
-                  borderRadius: '24px',
-                  fontWeight: 700
-                }}
-              >
-                <div className='formkit-spinner'>
-                  <div></div>
-                  <div></div>
-                  <div></div>
-                </div>
-                <span className=''>{form.buttonText}</span>
-              </button>
-            </div>
-            <div
-              className='formkit-guarantee'
-              data-element='guarantee'
-              style={{
-                color: 'rgb(77, 77, 77)',
-                fontSize: '13px',
-                fontWeight: 400
-              }}
-            >
-              <p>I won't spam you.</p>
-              <p>
-                Unsubscribe at <strong>any </strong>time.
-              </p>
-            </div>
+          <div
+            className='formkit-subheader dark:text-gray-300'
+            data-element='subheader'
+          // style={{ color: 'rgb(104, 104, 104)', fontSize: '15px' }}
+          >
+            <p>{form.subTitle}</p>
+          </div>
+          <div
+            className='formkit-image formkit-image relative focus:outline-none cursor-default'
+            role='button'
+          >
+            <img
+              className='focus:outline-blue'
+              src='https://embed.filekitcdn.com/e/8z8RseeFJ6cFGgheuTWKHN/rCC6cTJVbiDvbHM1MFZm6Z'
+              style={{ maxWidth: '100%' }}
+            />
           </div>
         </div>
-      </form>
-    </div>
+
+        <div data-element='column' className='formkit-column'>
+          <ul
+            className='formkit-alert formkit-alert-error'
+            data-element='errors'
+            data-group='alert'
+          ></ul>
+          <div data-element='fields' className='seva-fields formkit-fields'>
+            <div className='formkit-field'>
+              <input
+                className='formkit-input'
+                aria-label='First Name'
+                name='fields[first_name]'
+                required={true}
+                placeholder='First Name'
+                type='text'
+                style={{
+                  color: 'rgb(0, 0, 0)',
+                  borderColor: 'rgb(227, 227, 227)',
+                  borderRadius: '4px',
+                  fontWeight: 400
+                }}
+              />
+            </div>
+            <div className='formkit-field'>
+              <input
+                className='formkit-input'
+                name='email_address'
+                aria-label='Email Address'
+                placeholder='Email Address'
+                required={true}
+                type='email'
+                style={{
+                  color: 'rgb(0, 0, 0)',
+                  borderColor: 'rgb(227, 227, 227)',
+                  borderRadius: '4px',
+                  fontWeight: 400
+                }}
+              />
+            </div>
+            <button
+              data-element='submit'
+              className='formkit-submit formkit-submit bg-blue-500 hover:bg-blue-600 transition-colors duration-150'
+              style={{
+                borderRadius: '24px'
+              }}
+            >
+              <div className='formkit-spinner'>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+              <span className=''>{form.buttonText}</span>
+            </button>
+          </div>
+          <div
+            className='formkit-guarantee text-sm dark:text-gray-300'
+            data-element='guarantee'
+          >
+            <p>I won't spam you.</p>
+            <p>
+              Unsubscribe at <strong>any </strong>time.
+            </p>
+          </div>
+        </div>
+      </div>
+    </form>
   )
 }
